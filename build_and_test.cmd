4937c56a307b01120ef5845fe308f80ec83c38f0
Gururaj Deshpande <66385690+guru-desh@users.noreply.github.com>
2024-09-03 23:05:43 -0700

    F** add pdm build step

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -3,6 +3,7 @@ setlocal enabledelayedexpansion
 
 python -m pip install --quiet pdm
 pdm install --dev
+pdm build
 pdm run lint_and_format
 pdm run test
 
--- build_and_test.sh
+++ build_and_test.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 python -m pip install --quiet pdm
 pdm install --dev
+pdm build
 pdm run lint_and_format
 pdm run test
