1905912b098c889962434c7a9d6360fa8d1a7c37
Gururaj Deshpande <66385690+guru-desh@users.noreply.github.com>
2024-09-03 23:09:51 -0700

    F** update build and test (#21)
    * R** install dev dependencies
    
    * F** add pdm build step

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -2,7 +2,8 @@
 setlocal enabledelayedexpansion
 
 python -m pip install --quiet pdm
-pdm install
+pdm install --dev
+pdm build
 pdm run lint_and_format
 pdm run test
 
--- build_and_test.sh
+++ build_and_test.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 python -m pip install --quiet pdm
-pdm install
+pdm install --dev
+pdm build
 pdm run lint_and_format
 pdm run test
